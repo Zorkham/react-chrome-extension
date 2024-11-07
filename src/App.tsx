4352a8f98a6cd978142646a75da202233b6359bd
Zorkham <cedricmolla06@gmail.com>
2024-11-07 15:17:56 +0100

    init react chrome extension

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+import Popup from "./Popup";
+import React from "react";
+
+const App: React.FC = () => {
+  return <Popup />;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "./main.css";
+
+import App from "./App.tsx";
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
